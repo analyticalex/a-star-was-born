cb57d1da153fe5412efbeff5fef6925ca53d36ac
Samuel Gass <adahn6@gmail.com>
2017-03-08 20:44:42 -0800

    wow wow such first commit

---
+++ astar.py
@@ -0,0 +1,18 @@
+import sys
+
+class StarSolver(maze_file):
+
+
+def solveTheMaze(self, start_node):
+    closed_set = set()
+    open_set = set(start_node)
+    pass
+
+def heuristic_cost_estimate(self, neighbor, goal):
+    pass
+
+def read_maze_file(filename):
+    pass
+
+
+class GraphNode()
